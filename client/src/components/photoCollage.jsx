824cfbd31e035ea375f96ef9607b2a59b121513e
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-04-17 23:05:49 -0700

    Implement lazy loading and adjust first image height

--- client/src/components/photoCollage.jsx
+++ client/src/components/photoCollage.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import styles from '../styles/photoCollage.css';
+
+
+const PhotoCollage2 = React.lazy(() => import('./photoCollage2'));
+const PhotoCollage3 = React.lazy(() => import('./photoCollage3'));
 
 
 const PhotoCollage = ({
@@ -20,18 +24,20 @@ const PhotoCollage = ({
         <div className={styles.firstImageContainer}>
           <img className={styles.firstImage} src={dataArr.photosAndComments[0].imageUrl} onClick={() => {handleClickedImage(0, () => {toggleModal()})}} alt="host home" />
         </div>
-        <div className={styles.secondImageContainer}>
-          <img className={styles.secondImage} src={dataArr.photosAndComments[1].imageUrl} onClick={() => {handleClickedImage(1, () => {toggleModal()})}} alt="host home" />
-        </div>
-        <div className={styles.thirdImageContainer}>
-          <img className={styles.thirdImage} src={dataArr.photosAndComments[2].imageUrl} onClick={() => {handleClickedImage(2, () => {toggleModal()})}} alt="host home" />
-        </div>
-        <div className={styles.fourthImageContainer}>
-          <img className={styles.fourthImage} src={dataArr.photosAndComments[3].imageUrl} onClick={() => {handleClickedImage(3, () => {toggleModal()})}} alt="host home" />
-        </div>
-        <div className={styles.fifthImageContainer}>
-          <img className={styles.fifthImage} src={dataArr.photosAndComments[4].imageUrl} onClick={() => {handleClickedImage(4, () => {toggleModal()})}} alt="host home" />
-        </div>
+        <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
+          <PhotoCollage2
+            dataArr={dataArr}
+            toggleModal={toggleModal}
+            handleClickedImage={handleClickedImage}
+          />
+        </Suspense>
+        <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
+          <PhotoCollage3
+            dataArr={dataArr}
+            toggleModal={toggleModal}
+            handleClickedImage={handleClickedImage}
+          />
+        </Suspense>
       </div>
     </div>
   );
